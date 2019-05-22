81d8ebc656d1f2dd2cc14efab41cf39b63f4639d
Robert Rieger <robertrieger@zipcoders-MacBook-Pro-96.local>
2019-05-22 12:57:31 -0400

    completled sumOfInput Lab

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        Integer n = in.nextInt();
+        Integer sumOfNumbers = 0;
+        
+        
+        for( int i = 1; i <= n; i++){
+            sumOfNumbers += i;
+        }
+        System.out.println("The sum of the numbers beween 1 and " + n + " is " + sumOfNumbers);
 
     }
 }
